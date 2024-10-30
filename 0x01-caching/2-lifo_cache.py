5704246e72aa1fde3ac430d7e8fab8455a28ef68
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-30 22:01:31 +0300

    u

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,12 +14,14 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """Put a key/value pair"""
         if key and item:
-            self.cache_data[key] = item
+            if key and item:
+                self.cache_data[key] = item
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
-                print(f"DISCARD: {first_key}")
-                del self.cache_data[first_key]
+                keys = list(self.cache_data.keys())
+                second_to_last_key = keys[-2]
+                print(f"DISCARD: {second_to_last_key}")
+                del self.cache_data[second_to_last_key]
 
     def get(self, key):
         """Get a key/value pair"""

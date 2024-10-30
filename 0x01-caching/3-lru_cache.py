8056e41f75d5c2de136f0a2c95670c6a8779bd53
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-30 23:29:13 +0300

    t3.1

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
-"""cache module"""
+"""Least Recently Used caching module.
+"""
+from collections import OrderedDict
 
-
-from typing import OrderedDict
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """BasicCache class inherits from BaseCaching"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    """
 
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Put a key/value pair"""
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data.pop(key)
-
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
             self.cache_data[key] = item
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # oldest_key = next(iter(self.cache_data))
-                oldest_key, _ = self.cache_data.popitem(last=False)
-                print(f"DISCARD: {oldest_key}")
-
     def get(self, key):
-        """Get a key/value pair"""
-        if key:
-            return self.cache_data.get(key)
-        return None
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python3
-"""cache module"""
+"""Most Recently Used caching module.
+"""
+from collections import OrderedDict
 
-
-from typing import OrderedDict
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """BasicCache class inherits from BaseCaching"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
 
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Put a key/value pair"""
-
-        if key and item:
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
             self.cache_data[key] = item
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keys = list(self.cache_data.keys())
-            second_to_last_key = keys[-2]
-            print(f"DISCARD: {second_to_last_key}")
-            del self.cache_data[second_to_last_key]
-
     def get(self, key):
-        """Get a key/value pair"""
-        if key:
-            return self.cache_data.get(key)
-        return None
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)

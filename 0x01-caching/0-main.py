30bc53d78356035f94f539fae0d7d1798c49d667
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-30 21:24:51 +0300

    t1

--- 0x01-caching/0-main.py
+++ 0x01-caching/0-main.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" 0-main """
-BasicCache = __import__('0-basic_cache').BasicCache
+""" 2-main """
+LIFOCache = __import__('2-lifo_cache').LIFOCache
 
-my_cache = BasicCache()
-my_cache.print_cache()
+my_cache = LIFOCache()
 my_cache.put("A", "Hello")
 my_cache.put("B", "World")
 my_cache.put("C", "Holberton")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-print(my_cache.get("D"))
-my_cache.print_cache()
 my_cache.put("D", "School")
-my_cache.put("E", "Battery")
-my_cache.put("A", "Street")
 my_cache.print_cache()
-print(my_cache.get("A"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""cache module"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """BasicCache class inherits from BaseCaching"""
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Put a key/value pair"""
+        if key and item:
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                print(f"DISCARD: {first_key}")
+                del self.cache_data[first_key]
+
+    def get(self, key):
+        """Get a key/value pair"""
+        if key:
+            return self.cache_data.get(key)
+        return None

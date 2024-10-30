83ad26e2177464d068b399e597f7544c1297a012
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-30 22:26:04 +0300

    t3

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""cache module"""
+
+
+from typing import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """BasicCache class inherits from BaseCaching"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Put a key/value pair"""
+        if key and item:
+            if key and item:
+                self.cache_data[key] = item
+
+            # if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            #     oldest_key = next(iter(self.cache_data))
+            #     print(f"DISCARD: {oldest_key}")
+            #     del self.cache_data[oldest_key]
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                second_to_last_key = keys[-2]
+                print(f"DISCARD: {second_to_last_key}")
+                del self.cache_data[second_to_last_key]
+
+    def get(self, key):
+        """Get a key/value pair"""
+        if key:
+            return self.cache_data.get(key)
+        return None

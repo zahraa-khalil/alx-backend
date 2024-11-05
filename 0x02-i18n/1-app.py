33693c746dfe035c51fa1f718542e010c250867a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-11-05 18:40:15 +0200

    t1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,24 +2,21 @@
 """i18n Module"""
 
 
-from flask import Flask, render_template
-from flask_babel import Babel, _, format_datetime
-from datetime import datetime
+from flask import Flask
+from flask_babel import Babel
 
 app = Flask(__name__)
-
-
-babel = Babel(app)
 
 
 class Config:
     """language configuration"""
     LANGUAGES = ["en", "fr"]
+    app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+    app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
-
+app.config.from_object(Config)
+babel = Babel(app)
 
 if __name__ == '__main__':
     app.run(debug=True)

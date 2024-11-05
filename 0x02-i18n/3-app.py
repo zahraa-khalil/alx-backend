b99c69212588c161853af820cf80c0254646ec1d
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-11-05 19:37:01 +0200

    t3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""i18n Module"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """language configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """check language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    '''default route'''
+    return render_template("3-index.html",)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

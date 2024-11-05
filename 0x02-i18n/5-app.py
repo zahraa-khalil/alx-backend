5cd66e886c127b0d94b4734359e4a35c266aaab0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-11-05 23:44:50 +0200

    update

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -2,8 +2,12 @@
 """
 A Basic flask application
 """
+from typing import (
+    Dict, Union
+)
+
 from flask import Flask
-from flask import request
+from flask import g, request
 from flask import render_template
 from flask_babel import Babel
 
@@ -36,12 +40,39 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validate user login details
+    Args:
+        id (str): user id
+    Returns:
+        (Dict): user dictionary if id is valid else None
+    """
+    return users.get(int(id), 0)
+
+
+@app.before_request
+def before_request():
+    """
+    Adds valid user to the global session object `g`
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
